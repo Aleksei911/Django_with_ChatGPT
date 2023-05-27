8e09a7874ab3ce100dcbf5c99c216039efb0ba6b
Aleksei Kohnuk <kohnuk92@gmail.com>
2023-05-27 21:29:06 +0300

    added JS script in chat_bot.html and added openai

--- django_chatGPT_bot/chat_bot/views.py
+++ django_chatGPT_bot/chat_bot/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
+
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+
+def ask_openai(message):
+    reponse = openai.Completion.create(
+        model='text-davinci-003',
+        promt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = reponse.choices[0].text.strip()
+    return answer
 
 
 # Create your views here.
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'Hi this is my response'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chat_bot.html')
